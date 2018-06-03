157f816d2f59fa03fb51d22fd23f9a328497e25a
Paweł Kołodziejczak <modwebpro@gmail.com>
2018-06-03 18:51:11 +0200

    fix timing issue

--- preloader.js
+++ preloader.js
@@ -1,15 +1,15 @@
-export let preloader = function (el) {
-  this.init(el);
+export let preloader = function (el, time = '0.1') {
+  this.init(el, time);
 };
 
 preloader.prototype = {
   constructor: preloader,
 
-  init: function (el) {
+  init: function (el, time) {
     var _this = this;
 
     if (!_this._setVars(el)) return;
-    _this._setEvents();
+    _this._setEvents(time);
   },
 
   _setVars: function (el) {
@@ -23,12 +23,12 @@ preloader.prototype = {
     return true;
   },
 
-  _setEvents: function () {
+  _setEvents: function (time) {
     var _this = this;
 
-    TweenLite.killTweensOf(_this._preloader);
+    //TweenLite.killTweensOf(_this._preloader);
     window.addEventListener('load', function () {
-      TweenLite.to(_this._preloader, 0.1, {
+      TweenLite.to(_this._preloader, time, {
         autoAlpha: 0,
         onComplete: _this._cb,
         onCompleteParams: [_this]
@@ -37,10 +37,10 @@ preloader.prototype = {
   },
 
   _cb: function (_this) {
-
-    setTimeout(function () {
-      _this._preloader.remove();
+    let afterLoad = setTimeout(function () {
       console.log('load complete');
-    }, 500);
+      _this._preloader.remove();
+    }, time);
+    clearTimeout(afterLoad);
   }
 };

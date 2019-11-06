f825f37954b5139fdf3d88fa1de560e2249a4d25
Paweł Kołodziejczak <modwebpro@gmail.com>
2019-11-06 13:38:08 +0100

    Create preloader.js

---
+++ preloader.js
@@ -0,0 +1,29 @@
+export class preloader {
+  constructor(id, time = 0) {
+  
+    if (!this._setVars(id)) return;
+    TweenLite.to(this._preloader, time, {
+      autoAlpha: 0, onComplete: this.destroy
+    });
+    
+  }
+
+  _setVars(id) {
+  
+    this._preloader = typeof id === 'string' ? document.body.getElementsByClassName(id)[0] : id;
+    if (!this._preloader) return false;
+
+    return true;
+  }
+
+  destroy() {
+  
+    setTimeOut(() => {
+      if (!this._preloader || !this._preloader.parentNode) return;
+      this._preloader.parentNode.removeChild(this._preloader);
+    }, 100);
+    
+    for (const key in this) if (this.hasOwnProperty(key)) this[key] = null;
+  }
+  
+}
